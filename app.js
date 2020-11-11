99179908b332a8e97b23ed28fd0b6f866f5f2a04
Abygayle Ivey <abyivey@gmail.com>
2020-11-10 19:39:13 -0500

    Exercise 2

---
+++ app.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+
+    const btn = document.getElementById("search");
+    const textfield = document.getElementById("field");
+    const result = document.getElementById("result");
+    let httpRequest = new XMLHttpRequest();
+    let url = 'superheroes.php';
+  
+    btn.addEventListener('click', search);
+  
+  
+    function search(){
+      let data = textfield.value;
+      let ext = '?q='+data;
+      httpRequest.onreadystatechange = requestaction;
+      httpRequest.open('GET', url+ext, true);
+      httpRequest.send();
+    }
+    
+    function requestaction(){
+      if(httpRequest.readyState === XMLHttpRequest.DONE){
+        if(httpRequest.status === 200){
+  
+            let response = httpRequest.responseText;
+            result.innerHTML = response;
+  
+          }else{
+  
+            alert('There is a problem with the request');
+  
+          }
+      }
+    }
+  }
+  
+  
+  
+  
